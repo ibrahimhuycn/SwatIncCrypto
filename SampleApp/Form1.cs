30f8d63a064eb2ea162057b6137e586f282d44d5
Ibrahim Hussain <ibrahim.hucyn@live.com>
2020-06-20 20:16:13 +0500

    Created a sample application which generates a Hash

---
+++ SampleApp/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SampleApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Subscribe for events
+            textBoxPassword.KeyUp += GeneratePasswordHash;
+        }
+
+        private void GeneratePasswordHash(object sender, KeyEventArgs e)
+        {
+            richTextBoxGeneratedHash.Text = SwatIncCrypto.SwatIncCrypto.SwatIncSecurityCreateHashSHA512(textBoxPassword.Text);
+        }
+    }
+}
